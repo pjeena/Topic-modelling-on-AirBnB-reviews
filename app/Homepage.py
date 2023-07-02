9c5589c86c869ab0625be3776f3a0038d6dc4b87
Piyush Jeena <56345075+pjeena@users.noreply.github.com>
2023-07-02 12:20:25 +0200

    Update Homepage.py

--- app/Homepage.py
+++ app/Homepage.py
@@ -3,12 +3,13 @@ import pandas as pd
 import numpy as np
 import plotly.express as px
 import plotly.graph_objects as go
-#from nltk.tokenize import RegexpTokenizer
+
+# from nltk.tokenize import RegexpTokenizer
 import streamlit.components.v1 as components
 from PIL import Image
-#from annotated_text import annotated_text
-from bertopic import BERTopic
 
+# from annotated_text import annotated_text
+from bertopic import BERTopic
 
 
 st.set_page_config(
@@ -19,10 +20,13 @@ st.set_page_config(
 )
 
 st.title("Topic Modeling and Sentiment analysis on AirBnB reviews")
-st.write('*This is a dashboard based on the [AirBnB reviews](http://insideairbnb.com/get-the-data) \
-         in the city of Paris. 50000 reviews regarding hotel stays were analysed using BertTopic*')
+st.write(
+    "*This is a dashboard based on the [AirBnB reviews](http://insideairbnb.com/get-the-data) \
+         in the city of Paris. 50000 reviews regarding hotel stays were analysed using BertTopic*"
+)
 
-st.markdown("""
+st.markdown(
+    """
         <style>
                .block-container {
                     padding-top: 1rem;
@@ -31,58 +35,69 @@ st.markdown("""
                     padding-right: 5rem;
                 }
         </style>
-        """, unsafe_allow_html=True)
+        """,
+    unsafe_allow_html=True,
+)
+
 
 #################################################### Calculations #############################################################################
 @st.cache_data(experimental_allow_widgets=True)
 def load_model():
-
     topic_model = BERTopic.load("model/model_dir")
     return topic_model
 
+
 topic_model = load_model()
 
-#df = pd.read_parquet('data/paris_reviews_preprocessed.parquet')
+df = pd.read_parquet("data/paris_reviews_preprocessed.parquet")
 #################################################### Calculations #############################################################################
 
 
 ### top row
 
-#st.markdown("## ")
+# st.markdown("## ")
 
 # st.markdown("<hr/>", unsafe_allow_html=True)
 
+# st.markdown(
+#    """<hr style="height:5px;border:none;color:#333;background-color:#333;" /> """,
+#    unsafe_allow_html=True,
+# )
 
-#st.dataframe(df.head(), hide_index=True)
+
+# st.dataframe(df.head(), hide_index=True)
+
+# st.markdown(
+#    """<hr style="height:5px;border:none;color:#333;background-color:#333;" /> """,
+#    unsafe_allow_html=True,
+# )
 
 
-st.markdown(
-    "## Top 30 frequent topics"
+st.markdown("## Top 30 frequent topics")
+
+st.dataframe(
+    topic_model.get_topic_info()[1:30][["Topic", "Count", "Name", "Representation"]],
+    use_container_width=True,
 )
 
-st.dataframe(topic_model.get_topic_info()[1:30][['Topic', 'Count', 'Name', 'Representation']],use_container_width=True)
 
-
-st.markdown(
-    "## 2D representation of the topics"
+st.markdown("## 2D representation of the topics")
+st.plotly_chart(
+    topic_model.visualize_topics(top_n_topics=30),
+    theme="streamlit",
+    use_container_width=True,
 )
-st.plotly_chart( topic_model.visualize_topics(top_n_topics=30),theme='streamlit', use_container_width=True )
 
 
-
-st.markdown(
-    "## Topic similarities"
+st.markdown("## Topic similarities")
+st.plotly_chart(
+    topic_model.visualize_heatmap(top_n_topics=30),
+    theme="streamlit",
+    use_container_width=True,
 )
-st.plotly_chart( topic_model.visualize_heatmap(top_n_topics=30) ,theme='streamlit', use_container_width=True )
 
 
-
-st.markdown(
-    "## Visualizing individual terms of the top 8 topics"
+st.markdown("## Visualizing individual terms of the top 8 topics")
+st.plotly_chart(
+    topic_model.visualize_barchart(), theme="streamlit", use_container_width=True
 )
-st.plotly_chart( topic_model.visualize_barchart(),theme='streamlit', use_container_width=True )
-
-
-
-
-
